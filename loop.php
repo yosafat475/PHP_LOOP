1ca12bf9c1974caa26b436a04ffdcc8bf6924348
yosafat475 <70566441+yosafat475@users.noreply.github.com>
2020-09-26 09:10:13 +0700

    Add files via upload

---
+++ loop.php
@@ -0,0 +1,184 @@
+<?php
+
+
+//foreach($siswa as $key){
+//     echo $key ."\n";
+// }
+
+// function ulang($nomor){
+
+// for($a=0;$a<=$nomor;$a++){
+//    echo $a;
+// }
+// }
+
+// $number=1;           //ini bentuk loping while
+// while($number<=10){
+//    echo $number.PHP_EOL;
+//    $number++;
+// }
+
+// $number2=1;
+
+
+// do{                 //ini bentuk loping dari do while
+//    echo $number2.PHP_EOL;
+//    $number2++;
+// }while($number2<=10);
+
+// do{    
+//    echo "tampilkan ini";
+
+//    $berhenti=readline("lagi ?");
+
+//    if($berhenti=="ya"){
+
+//    break;
+//    }
+// }while(true);
+
+// while(true){
+//    echo "tampil tulisan\n";
+//    $berhenti=readline("keluar dari loop ?");
+//    if($berhenti=="ya"){
+//    break;
+//    }
+// }
+
+// for($x=10;$x>=0;$x--){//kalau naik urut
+//    echo $x;
+// }
+// for($x=10;$x>=0;$x-=2){// kalau naik turun loncat 2
+//    echo $x."\n";
+// }
+// for($x=0;$x<=10;$x+=2){// naik loncat 2
+//    echo $x;
+// }
+// for($x=0;$x<=10;$x=$x+2){//sama naik loncat 2 tapi dengan fersi beda
+//    echo $x."\n";
+// }
+
+// function nomor1(){
+// for($x=1;$x<=10;$x++){
+//     if($x==4){
+//         continue;
+//     }elseif($x==5){
+//         continue;
+//     }elseif($x==6){
+//         continue;
+//     }elseif($x==7){
+//         continue;
+//     }
+//     echo $x;
+// }
+// }
+// cara singkat no 1
+// hasil 1238910
+    // for($x=1;$x<=10;$x++){
+    //     if($x>3 && $x<8){
+    //         echo "";
+    //     }else{
+    //     echo "$x";
+    //     }
+    // }
+
+// nomer 6
+//hasil AA234
+// for($t=0;$t<=4;$t++){
+//     if($t==0 || $t==1){
+//         echo "\nA";
+//         continue;
+//     }
+//     echo "\n$t";
+// }
+// ada cara lagi yg no 6
+// for($l=0;$l<=4;$l++){
+//     if($l<2){
+//         echo "A";
+//         // continue;
+//     }else{
+//     echo $l;
+//     }
+// }
+
+// hasil 1238910
+// $number=1;           
+// while($number<=10)
+// {
+//         if($number>3 && $number<8)
+//         {
+//             echo "";
+//         }else{
+//         echo "$number";
+//         }
+//         $number++;
+// }
+// nomor 2
+// hasil 543**
+// for($x=5;$x>=1;$x--){
+//     if($x==2 || $x==1){
+//         echo "*";
+//         continue;
+//     }   
+//     echo $x;
+// }
+
+
+    //  hasill  1B2A3B4A
+    // ini yg bukn modul
+//  for($x=1;$x<=9;$x++){
+//      if($x==1 || $x==7){//jika 1 atau 3 sama dengan a lah kalau soal nya sampai 1-8 dst pakek modul
+//          echo $x."A";//modul itu membaca sisa hasil ya itu pakek %
+//      }else{
+//          echo $x."B";
+//      }
+//  }
+
+//  yg ini modul
+
+
+// for($x=1;$x<=9;$x++){
+//     if($x %2==1){
+//         echo $x."A";
+//     }else{
+//         echo $x."B";
+//     }
+// }
+
+
+// ini nomor 4
+// hasil 123AAA7
+// for($z=1;$z<=7;$z++){
+//     if($z>3 && $z<7){
+//         echo "A";
+//         continue;
+//     }
+//     echo $z;
+// }
+
+// ini nomor 5
+// hasil  43A1A
+//    for($q=4;$q>=0;$q--){
+//     if($q<3)
+//     {
+//         if($q==1){
+//             echo "$q";
+//         }else{
+//             echo "A";
+//         }
+//     }else{
+//     echo $q;
+//     }
+    
+// }
+
+
+
+// ini soal untuk mas nanda
+
+// function raepakek($a,$b){
+//     return ($a*$b);
+// }
+// echo raepakek(2,3);
+
+
